888f9a16a885a35e27aa6d5f8234fcea1fa2753a
Christine Shin <christine@Christines-MacBook-Pro.local>
2020-07-21 11:39:58 +0900

    [Add] CommunityMain, CommunityForm page

--- frontend/src/plugins/vuetify.js
+++ frontend/src/plugins/vuetify.js
@@ -4,4 +4,14 @@ import Vuetify from 'vuetify/lib';
 Vue.use(Vuetify);
 
 export default new Vuetify({
+  // color theme 설정 
+  theme: {
+    themes: {
+      light: {
+        primary: '#c1666b',
+        secondary: '#e4dfda',
+        accent: '#d4b483',
+      },
+    },
+  },
 });
---
+++ frontend/src/views/community/CommunityForm.vue
@@ -0,0 +1,66 @@
+<template>
+  <v-container>
+    <h1 class="text-center">New</h1>
+
+    <!-- title -->
+    <v-row class="justify-center">
+      <v-col class="py-0" sm="10">
+        <v-text-field
+          class="my-3"
+          hide-details="true"
+          v-model="articleData.title"
+          id="title"
+          placeholder="제목"
+          type="text"
+          outlined
+        />
+      </v-col>
+    </v-row>
+
+    <!-- content -->
+    <v-row class="justify-center">
+      <v-col class="py-0" sm="10">
+        <v-textarea
+          class="my-3"
+          hide-details="true"
+          v-model="articleData.content"
+          id="content"
+          placeholder="내용"
+          type="text"
+          outlined
+        ></v-textarea>
+      </v-col>
+    </v-row>
+
+    <v-row class="justify-center">
+      <v-col class="py-0 text-right" sm=10>
+        <v-btn
+          @click="createArticle({title: articleData.title, content: articleData.content})"
+          :disabled="!isSubmit"
+          :class="{disabled : !isSubmit}"
+          depressed
+          large
+          color="accent"
+        >글쓰기</v-btn>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "CommunityForm",
+
+  data() {
+    return {
+      articleData: {
+        title: null,
+        content: null
+      }
+    };
+  }
+};
+</script>
+
+<style>
+</style>
