eee91ef7597f8969fd2d7e8e595ba4a9892781a7
multicampus <multicampus@DESKTOP-KVCQHCD>
2020-07-24 08:41:38 +0900

    pull

--- backend/indieAndBob/src/main/java/com/ssafy/indieAndBob/game/service/GameService.java
+++ backend/indieAndBob/src/main/java/com/ssafy/indieAndBob/game/service/GameService.java
@@ -1,9 +1,12 @@
 package com.ssafy.indieAndBob.game.service;
 
+import java.util.List;
+
 import com.ssafy.indieAndBob.game.dto.Game;
 
 public interface GameService {
 
 	Game selectGameById(String gameId);
 	int registerGame(Game game);
+	List<Game> selectAllGame();
 }
--- backend/indieAndBob/src/main/java/com/ssafy/indieAndBob/reward/service/RewardService.java
+++ backend/indieAndBob/src/main/java/com/ssafy/indieAndBob/reward/service/RewardService.java
@@ -1,8 +1,12 @@
 package com.ssafy.indieAndBob.reward.service;
 
+import java.util.List;
+
 import com.ssafy.indieAndBob.reward.dto.Reward;
 
 public interface RewardService {
 	int registerReward(Reward reward);
 	Reward selectRewardById(String rewardId);
+	List<Reward> selectAllReward();
+	List<Reward> selectRewardByGameId(String gameId);
 }
