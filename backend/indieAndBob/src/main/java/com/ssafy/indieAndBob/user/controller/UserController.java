5beece6886998c501e82754647a863aed878da0d
multicampus <multicampus@DESKTOP-KVCQHCD>
2020-07-23 15:46:26 +0900

    [edit] UserController.java
    add logger

--- backend/indieAndBob/src/main/java/com/ssafy/indieAndBob/user/controller/UserController.java
+++ backend/indieAndBob/src/main/java/com/ssafy/indieAndBob/user/controller/UserController.java
@@ -2,6 +2,8 @@ package com.ssafy.indieAndBob.user.controller;
 
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,6 +23,8 @@ import io.swagger.annotations.ApiOperation;
 @CrossOrigin(origins = { "http://localhost:3000" })
 @RestController
 public class UserController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 	@Autowired
 	UserService userService;
 	@Autowired
@@ -29,6 +33,7 @@ public class UserController {
 	@PostMapping("/account/login")
 	@ApiOperation(value = "로그인")
 	public Object login(@RequestBody User user, HttpServletResponse res) {
+		logger.info("==========login==========");
 		ResponseEntity response = null;
 		User u = userService.selectByEmailAndPassword(user);
 		if (u != null) {
@@ -48,6 +53,7 @@ public class UserController {
 	@PostMapping("/account/signup")
 	@ApiOperation(value = "가입하기")
 	public Object signup(@RequestBody User request) {
+		logger.info("==========signup==========");
 		ResponseEntity response = null;
 		User u = userService.selectByEmail(request.getEmail());
 		if (u == null) {
@@ -71,6 +77,7 @@ public class UserController {
 	@PutMapping("/account/userinfo")
 	@ApiOperation(value = "회원 정보 수정")
 	public Object changeUserInfo(@RequestBody User request) {
+		logger.info("==========changeUserInfo==========");
 		ResponseEntity response = null;
 		if (userService.changeUserInfo(request) == 1) {
 			final BasicResponse result = new BasicResponse();
