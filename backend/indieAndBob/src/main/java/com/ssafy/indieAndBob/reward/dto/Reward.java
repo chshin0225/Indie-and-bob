df4843a4729a6eabcdc21abb4eb4046a2e703f15
multicampus <multicampus@DESKTOP-KVCQHCD>
2020-07-23 10:46:57 +0900

    [pull] backend

---
+++ backend/indieAndBob/src/main/java/com/ssafy/indieAndBob/reward/dto/Reward.java
@@ -0,0 +1,19 @@
+package com.ssafy.indieAndBob.reward.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Reward {
+
+	public String rewardId;
+	public String gameId;
+	public String rewardName;
+	public String rImg;
+	public int leftPrice;
+	public int price;
+	public String content;
+}
