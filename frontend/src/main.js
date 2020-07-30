e4157bfc85076aaeab6759a173b115cf45b99135
multicampus <multicampus@DESKTOP-KVCQHCD>
2020-07-30 11:12:40 +0900

    pull

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,10 +4,12 @@ import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify';
 import VueSimpleAlert from "vue-simple-alert";
-
+import VueMoment from 'vue-moment'
+import Clipboard from 'v-clipboard'
+import socketio from 'socket.io-client'
+import VueSocketIO from 'vue-socket.io'
 
 Vue.config.productionTip = false
-
 
 new Vue({
   router,
@@ -17,3 +19,14 @@ new Vue({
 }).$mount('#app')
 
 Vue.use(VueSimpleAlert);
+Vue.use(VueMoment);
+Vue.use(Clipboard);
+Vue.use(new VueSocketIO({
+  debug: true,
+  connection: socketio('http://localhost:8080'),
+  vuex: {
+    store,
+    actionPrefix: 'SOCKET_',
+    mutationPrefix: 'SOCKET_'
+  }
+}))
