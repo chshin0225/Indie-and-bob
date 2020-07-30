d527b035506562fd51969456db32057ca17e9c0b
박희웅 <pandabak@naver.com>
2020-07-30 16:47:26 +0900

    [Recover] indexfeed

--- frontend/src/views/IndexFeed.vue
+++ frontend/src/views/IndexFeed.vue
@@ -1,11 +1,14 @@
 <template>
   <v-container>
+    <h1>Main page</h1>
     <div>
-      <router-link to="/user/mypage">마이페이지</router-link>
-      <br>
-      <router-link to="/community">커뮤니티</router-link>
-      <router-link to="/project/new">새 프로젝트 만들기</router-link>
-
+      <router-link to="/newproject">새 프로젝트 만들기</router-link>
+    </div>
+    <div>
+      <router-link to="/user/mypage/test100">test100</router-link><br>
+      <router-link to="/user/mypage/test200">test200</router-link><br>
+      <router-link to="/user/mypage/test300">test300</router-link><br>
+      <router-link to="/user/mypage/test400">test400</router-link>
     </div>
   </v-container>
 </template>
