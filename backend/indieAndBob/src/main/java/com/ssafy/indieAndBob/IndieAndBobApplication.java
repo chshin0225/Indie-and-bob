9dfc3d6616b2e4efdc1cbe5918fdc04238496ad2
multicampus <multicampus@DESKTOP-KVCQHCD>
2020-07-30 10:50:51 +0900

    [edit] game islike

--- backend/indieAndBob/src/main/java/com/ssafy/indieAndBob/IndieAndBobApplication.java
+++ backend/indieAndBob/src/main/java/com/ssafy/indieAndBob/IndieAndBobApplication.java
@@ -3,16 +3,20 @@ package com.ssafy.indieAndBob;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 import com.ssafy.indieAndBob.jwt.interceptor.JwtInterceptor;
 
-import io.jsonwebtoken.lang.Arrays;
-
 @SpringBootApplication
 public class IndieAndBobApplication implements WebMvcConfigurer {
+	
+	@Bean
+    public ServerEndpointExporter serverEndpointExporter() {
+        return new ServerEndpointExporter();
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(IndieAndBobApplication.class, args);
