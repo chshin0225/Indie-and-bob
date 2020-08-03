1b375d6948982fac485877831bc6413456cc8292
multicampus <anjimmy@naver.com>
2020-08-03 10:41:56 +0900

    Edit kakaopay

--- backend/indieAndBob/src/main/java/com/ssafy/indieAndBob/kakaopay/controller/KakaopayController.java
+++ backend/indieAndBob/src/main/java/com/ssafy/indieAndBob/kakaopay/controller/KakaopayController.java
@@ -1,6 +1,7 @@
 package com.ssafy.indieAndBob.kakaopay.controller;
 
 
+import com.ssafy.indieAndBob.kakaopay.dto.Funding;
 import com.ssafy.indieAndBob.kakaopay.dto.KakaoPayApprovalVO;
 import com.ssafy.indieAndBob.kakaopay.service.KakaoPay;
 import com.ssafy.indieAndBob.response.dto.BasicResponse;
@@ -14,6 +15,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,10 +32,11 @@ public class KakaopayController {
     
     @PostMapping("/kakaoPay")
     @ApiOperation(value = "결제하기")
-    public Object kakaoPay() {
+    public Object kakaoPay(@RequestBody Funding request) {
         log.info("kakaoPay post............................................");
+        log.info("funding : " + request);
         ResponseEntity response = null;
-        String toPay = kakaopay.kakaoPayReady();
+        String toPay = kakaopay.kakaoPayReady(request);
 		if( toPay != null) {
 			final BasicResponse result = new BasicResponse();
 			result.status = true;
@@ -48,10 +51,12 @@ public class KakaopayController {
     
     @GetMapping("/kakaoPaySuccess")//결제가된다면 url에 pg_token이 포함되어있을것이다
     @ApiOperation(value = "결제내역")
-    public Object kakaoPaySuccess(@RequestParam("pg_token") String pg_token, Model model) {
+    public Object kakaoPaySuccess(@RequestParam("pg_token") String pg_token,@RequestParam("nickname") String nickname,
+    		@RequestParam("orderId") int orderId,
+    		@RequestParam("amount") int amount,Model model) {
         log.info("kakaoPaySuccess get............................................");
         log.info("kakaoPaySuccess pg_token : " + pg_token);
-        KakaoPayApprovalVO info = kakaopay.kakaoPayInfo(pg_token);
+        KakaoPayApprovalVO info = kakaopay.kakaoPayInfo(pg_token,nickname,orderId,amount);
         model.addAttribute("info", info);
        // model.addAttribute("info",kakaopay.kakaoPayInfo(pg_token));//정보들
         ResponseEntity response = null;
