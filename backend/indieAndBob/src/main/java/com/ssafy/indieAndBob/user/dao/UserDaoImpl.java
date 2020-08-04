ee1de96ed00aeb37b8753d6fb73f55f8e870a11c
multicampus <multicampus@DESKTOP-KVCQHCD>
2020-08-04 14:54:11 +0900

    [edit] isfollowing, email auth

--- backend/indieAndBob/src/main/java/com/ssafy/indieAndBob/user/dao/UserDaoImpl.java
+++ backend/indieAndBob/src/main/java/com/ssafy/indieAndBob/user/dao/UserDaoImpl.java
@@ -59,7 +59,7 @@ public class UserDaoImpl implements UserDao {
 	@Override
 	public boolean isFollowing(Follow follow) {
 		Follow f = temp.selectOne(ns + "isFollowing", follow);
-		if(f.equals(null)) return false;
+		if(f == null) return false;
 		return true;
 	}
 
