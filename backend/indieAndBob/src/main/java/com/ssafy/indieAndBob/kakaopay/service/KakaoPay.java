0c8ab18a4e15d6d26bd7d0877ed208b49516396d
multicampus <multicampus@DESKTOP-KVCQHCD>
2020-08-05 10:19:55 +0900

    [edit] kakao pay, funding, left_price of game

--- backend/indieAndBob/src/main/java/com/ssafy/indieAndBob/kakaopay/service/KakaoPay.java
+++ backend/indieAndBob/src/main/java/com/ssafy/indieAndBob/kakaopay/service/KakaoPay.java
@@ -6,7 +6,10 @@ import java.net.URISyntaxException;
 import com.ssafy.indieAndBob.kakaopay.dto.Funding;
 import com.ssafy.indieAndBob.kakaopay.dto.KakaoPayApprovalVO;
 import com.ssafy.indieAndBob.kakaopay.dto.KakaoPayReadyVO;
+import com.ssafy.indieAndBob.reward.dto.Reward;
+import com.ssafy.indieAndBob.reward.service.RewardService;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -25,6 +28,8 @@ public class KakaoPay {
     private static final String HOST = "https://kapi.kakao.com";
     
     private KakaoPayReadyVO kakaoPayReadyVO;
+    @Autowired
+    RewardService rewardService;
     
     //결제화면
     @SuppressWarnings("deprecation")
@@ -41,14 +46,16 @@ public class KakaoPay {
         // 서버로 요청할 Body
         MultiValueMap<String, String> params = new LinkedMultiValueMap<String, String>();
         params.add("cid", "TC0ONETIME");//테스트용 cid지정되어있음
-        params.add("partner_order_id", Integer.toString(request.getFundingId()));//주문아이디
+        params.add("partner_order_id", Integer.toString(request.getRewardId()));//주문아이디
         params.add("partner_user_id", request.getNickname());//유저아이디
-        params.add("item_name", request.getRewardName());//제품명
+        Reward reward = rewardService.selectRewardById(request.getRewardId());
+        String rewardName = reward.getRewardName();
+        params.add("item_name", rewardName);//제품명
         params.add("quantity", "1");//수량
         params.add("total_amount", Integer.toString(request.getMoney()));//가격
         params.add("tax_free_amount", "100");//세금
         //params.add("approval_url", "http://i3a105.p.ssafy.io:3000/home");//승인시갈곳
-        params.add("approval_url", "http://i3a105.p.ssafy.io:8080/kakaoPaySuccess?nickname="+request.getNickname()+"&orderId="+Integer.toString(request.getFundingId())+"&amount="+Integer.toString(request.getMoney()));
+        params.add("approval_url", "http://i3a105.p.ssafy.io:8080/kakaoPaySuccess?nickname="+request.getNickname()+"&gameId="+Integer.toString(request.getGameId())+"&rewardId="+Integer.toString(request.getRewardId())+"&money="+Integer.toString(request.getMoney()));
         params.add("cancel_url", "http://i3a105.p.ssafy.io:3000");//취소시갈곳
         params.add("fail_url", "http://i3a105.p.ssafy.io:3000");//실패시갈곳
  
