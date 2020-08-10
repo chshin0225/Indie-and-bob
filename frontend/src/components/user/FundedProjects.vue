846317e13e29cb48dd47ea7bfda351a439f4bbaa
Christine Shin <christine@Christines-MacBook-Pro.local>
2020-08-10 14:51:46 +0900

    [Edit] qna

--- frontend/src/components/user/FundedProjects.vue
+++ frontend/src/components/user/FundedProjects.vue
@@ -1,7 +1,31 @@
 <template>
   <v-container>
-    <h2 class="text-center">펀딩한 프로젝트들</h2>
-    <p>{{ fundedProjectList }}</p>
+    <v-row>
+      <v-col v-for="project in fundedProjectList" :key="project.gameId" cols=6 sm=4>
+        <v-card>
+          <router-link :to="`/game/${project.gameId}`" class="text-decoration-none">
+              <v-list-item>
+                <v-avatar>
+                  <img src="../../assets/default_profile.png" :alt="project.nickname" />
+                </v-avatar>
+                <v-list-item-content class="ml-4">
+                  <v-list-item-title class="headline">{{ project.name }}</v-list-item-title>
+                  <router-link class="text-decoration-none" :to="`/user/mypage/${project.nickname}`">{{ project.nickname }}</router-link>
+                  <v-list-item-subtitle>{{ $moment(project.deadline).format('YYYY.MM.DD') }}까지</v-list-item-subtitle>
+                  <v-list-item-subtitle>목표액: {{ project.aim }}</v-list-item-subtitle>
+                </v-list-item-content>
+              </v-list-item>
+              <v-img src="../../assets/default_project.png" height="194"></v-img>
+          </router-link>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn icon>
+              <v-icon>mdi-heart</v-icon>
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
