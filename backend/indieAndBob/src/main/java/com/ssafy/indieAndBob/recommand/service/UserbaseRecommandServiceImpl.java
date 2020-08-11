16058c33b5599325e8f12827be33b6cac5a5aeac
multicampus <multicampus@DESKTOP-KVCQHCD>
2020-08-11 17:53:26 +0900

    [add] userbaseRecommand

---
+++ backend/indieAndBob/src/main/java/com/ssafy/indieAndBob/recommand/service/UserbaseRecommandServiceImpl.java
@@ -0,0 +1,22 @@
+package com.ssafy.indieAndBob.recommand.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ssafy.indieAndBob.recommand.dao.UserBaseRecommandDao;
+import com.ssafy.indieAndBob.recommand.dto.UserbaseRecommand;
+
+@Service
+public class UserbaseRecommandServiceImpl implements UserbaseRecommandService {
+
+	@Autowired
+	UserBaseRecommandDao ubDao;
+	
+	@Override
+	public List<UserbaseRecommand> userbaseRecommand() {
+		return ubDao.userbaseRecommand();
+	}
+
+}
