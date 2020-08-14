f01b5b865c1ec612a4550f604326b08b749df3d2
multicampus <multicampus@DESKTOP-KVCQHCD>
2020-08-14 09:29:46 +0900

    [edit]

--- backend/indieAndBob/src/test/java/com/ssafy/indieAndBob/IndieAndBobApplicationTests.java
+++ backend/indieAndBob/src/test/java/com/ssafy/indieAndBob/IndieAndBobApplicationTests.java
@@ -1,13 +1,13 @@
-//package com.ssafy.indieAndBob;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//@SpringBootTest
-//class IndieAndBobApplicationTests {
-//
-//	@Test
-//	void contextLoads() {
-//	}
-//
-//}
+package com.ssafy.indieAndBob;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndieAndBobApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
