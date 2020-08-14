5e4526291abd8bf311d27298552479534dc85366
박희웅 <pandabak@naver.com>
2020-08-14 17:34:24 +0900

    [Edit]

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -225,6 +225,7 @@ export default new Vuex.Store({
     login({ commit }, loginData) {
       axios.post(SERVER.BASE + SERVER.LOGIN, loginData)
         .then(res => {
+          if (res.data.status) {
           commit('setEmail', res.data.object.email)
           commit('setPassword', res.data.object.password)
 
@@ -235,14 +236,11 @@ export default new Vuex.Store({
           commit('setToken', res.headers['jwt-auth-token'])
           
           router.push({ name: 'Home' })
-        })
-        .catch(err => {
-          if (err.response.status === 404) {
-            router.push({ name: "PageNotFound" })
-          } else {
-            console.error(err)
-          }
-        })
+        } else {
+          alert(res.data.data)
+        }
+      })
+        .catch(() => {})
     },
 
     SignUp({ commit, state }, signupData) {
@@ -257,47 +255,22 @@ export default new Vuex.Store({
         firebase.storage().ref(`user/${signupData.nickname}/${signupData.nickname}.${extension}`).put(signupData.profile)
         signupData.profile = `user/${signupData.nickname}/${signupData.nickname}.${extension}`
       }
-      if (signupData.genreId !== null) {
-        let genreArray = []
-        signupData.genreId.forEach(item => {
-          genreArray.push(state.genreToId[item])
-        })
-        signupData.genreId = genreArray
-      console.log(signupData)
+      let genreArray = []
+      signupData.genreId.forEach(item => {
+        genreArray.push(state.genreToId[item])
+      })
+      signupData.genreId = genreArray
       axios.post(SERVER.BASE + SERVER.SIGNUP, signupData)
       .then(res => {
-        console.log(signupData)
         console.log(res)
         if (res.data.status) {
-          alert("회원가입 인증 메일이 발송되었습니다. 이메일을 확인해주세요.")
+          alert("회원가입이 완료되었습니다.")
           router.push({ name: "Login" });
         } else {
-          // console.log(res.data.status)
-          commit('setErrorDetail', res.data.data)
-          router.push({ name: "ErrorPage" })
+          alert(res.data.data)
         }
         })
-        .catch(err => {
-          console.log(err.response)
-        })
-      }
-      else {
-        axios.post(SERVER.BASE + SERVER.SIGNUP, signupData)
-          .then(res => {
-            // console.log(res)
-            if (res.data.status) {
-              alert("회원가입 인증 메일이 발송되었습니다. 이메일을 확인해주세요.")
-              router.push({ name: "Login" });
-            } else {
-              // console.log(res.data.status)
-              commit('setErrorDetail', res.data.data)
-              router.push({ name: "ErrorPage" })
-            }
-          })
-          .catch(err => {
-            console.log(err.response)
-          })
-      }
+        .catch(() => {})
     },
 
     logout({ commit}) {
