1a65f91d623f041a1bf6c0baeb0d08bad1daed0c
multicampus <multicampus@host.docker.internal>
2020-08-18 10:48:18 +0900

    [edit] user base

--- backend/indieAndBob/src/main/java/com/ssafy/indieAndBob/recommand/service/UserbaseRecommandServiceImpl.java
+++ backend/indieAndBob/src/main/java/com/ssafy/indieAndBob/recommand/service/UserbaseRecommandServiceImpl.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.ssafy.indieAndBob.game.dao.GenreDao;
 import com.ssafy.indieAndBob.game.dto.GameAll;
 import com.ssafy.indieAndBob.recommand.dao.UserBaseRecommandDao;
 import com.ssafy.indieAndBob.recommand.dto.UserbaseRecommand;
@@ -14,7 +15,8 @@ public class UserbaseRecommandServiceImpl implements UserbaseRecommandService {
 
 	@Autowired
 	UserBaseRecommandDao ubDao;
-	
+	@Autowired
+	GenreDao genredao;
 	@Override
 	public List<UserbaseRecommand> userbaseRecommand() {
 		return ubDao.userbaseRecommand();
@@ -27,7 +29,12 @@ public class UserbaseRecommandServiceImpl implements UserbaseRecommandService {
 
 	@Override
 	public List<GameAll> randomRecommend() {
-		return ubDao.randomRecommend();
+		List<GameAll> games = ubDao.randomRecommend();
+		for(GameAll game : games) {
+			List<String> genreName = genredao.selectGenreNameByGameId(game.getGameId());
+			game.setGenreName(genreName);
+		}
+		return games;
 	}
 	
 }
