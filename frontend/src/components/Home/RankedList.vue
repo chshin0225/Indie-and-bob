de9c7e5ef3deadf2cbe49a23cfc808bba58676b6
박희웅 <pandabak@naver.com>
2020-08-19 18:03:06 +0900

    [Edit] comma

--- frontend/src/components/Home/RankedList.vue
+++ frontend/src/components/Home/RankedList.vue
@@ -6,8 +6,8 @@
           <v-list-item-content class="py-0">
             <v-col cols="7" class="px-0">
               <v-list-item-subtitle class="font-weight-bold black--text text-subtitle-1">{{ project.name }}</v-list-item-subtitle>
-              <v-list-item-subtitle>{{ project.genreName }}</v-list-item-subtitle>
-              <v-list-item-subtitle class="mt-1"> {{ project.leftDay }}일 남음 <span class="primary--text">{{ fundingProgress(project.aim, project.leftPrice) }}% 달성</span></v-list-item-subtitle>
+              <v-list-item-subtitle class="mt-2">{{ project.genreName }}</v-list-item-subtitle>
+              <v-list-item-subtitle > {{ project.leftDay }}일 남음 <span class="primary--text">{{ fundingProgress(project.aim, project.leftPrice) }}% 달성</span></v-list-item-subtitle>
             </v-col>
             <v-col cols="5">
               <v-img max-height="70px" :src="project.thumbnail"></v-img>
